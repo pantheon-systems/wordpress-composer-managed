42449753df68c88be356cd35d6bc07e31e0c7ccc
Scott Walkinshaw <scott.walkinshaw@gmail.com>
2013-12-20 18:46:58 -0500

    Add web/ folder

--- web/index.php
+++ web/index.php
@@ -1,6 +1,4 @@
 <?php
-/**
- * WordPress View Bootstrapper
- */
+// WordPress view bootstrapper
 define('WP_USE_THEMES', true);
-require __DIR__ . '/wp/wp-blog-header.php';
+require(dirname( __FILE__ ) . '/wp/wp-blog-header.php');
