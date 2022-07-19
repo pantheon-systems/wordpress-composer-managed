58b87fe4dbd916b58c510b1f5be76eb31c3f8754
jspellman814 <john.spellman@pantheon.io>
2022-07-19 14:27:51 -0600

    Fix up wp-config-pantheon

--- web/wp-config-pantheon.php
+++ web/wp-config-pantheon.php
@@ -14,42 +14,49 @@ $rootPath = realpath( __DIR__ . '/..' );
  */
 require_once( $rootPath . '/vendor/autoload.php' );
 
-/*
- * Fetch .env
- */
-if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) && file_exists( $rootPath . '/.env' ) ) {
-	$dotenv = Dotenv\Dotenv::create($rootPath);
-	$dotenv->load();
-	$dotenv->required( array(
-		'DB_NAME',
-		'DB_USER',
-		'DB_HOST',
-	) )->notEmpty();
-}
+// ** MySQL settings - included in the Pantheon Environment ** //
+/** The name of the database for WordPress */
+define( 'DB_NAME', $_ENV['DB_NAME'] );
 
-/**
- * Disallow on server file edits
- */
-define( 'DISALLOW_FILE_EDIT', true );
-define( 'DISALLOW_FILE_MODS', true );
+/** MySQL database username */
+define( 'DB_USER', $_ENV['DB_USER'] );
 
-/**
- * Force SSL
- */
-define( 'FORCE_SSL_ADMIN', true );
+/** MySQL database password */
+define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
 
-/**
- * Limit post revisions
- */
-define( 'WP_POST_REVISIONS', 3 );
+/** MySQL hostname; on Pantheon this includes a specific port number. */
+define( 'DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] );
 
-/*
- * If NOT on Pantheon
+/** Database Charset to use in creating database tables. */
+define( 'DB_CHARSET', 'utf8' );
+
+/** The Database Collate type. Don't change this if in doubt. */
+define( 'DB_COLLATE', '' );
+
+/**#@+
+ * Authentication Unique Keys and Salts.
+ *
+ * Change these to different unique phrases!
+ * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
+ * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
+ *
+ * Pantheon sets these values for you also. If you want to shuffle them you
+ * can do so via your dashboard.
+ *
+ * @since 2.6.0
  */
-if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
-	/**
-	 * Define site and home URLs
-	 */
+define( 'AUTH_KEY', $_ENV['AUTH_KEY'] );
+define( 'SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY'] );
+define( 'LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY'] );
+define( 'NONCE_KEY', $_ENV['NONCE_KEY'] );
+define( 'AUTH_SALT', $_ENV['AUTH_SALT'] );
+define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
+define( 'LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT'] );
+define( 'NONCE_SALT', $_ENV['NONCE_SALT'] );
+/**#@-*/
+
+/** A couple extra tweaks to help things run well on Pantheon. **/
+if ( isset( $_SERVER['HTTP_HOST'] ) ) {
 	// HTTP is still the default scheme for now.
 	$scheme = 'http';
 	// If we have detected that the end use is HTTPS, make sure we pass that
@@ -58,113 +65,64 @@ if ( ! isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
 	if ( isset( $_SERVER['HTTP_USER_AGENT_HTTPS'] ) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON' ) {
 		$scheme = 'https';
 	}
-	$site_url = getenv( 'WP_HOME' ) !== false ? getenv( 'WP_HOME' ) : $scheme . '://' . $_SERVER['HTTP_HOST'] . '/';
-	define( 'WP_HOME', $site_url );
-	define( 'WP_SITEURL', $site_url . 'wp/' );
+	define( 'WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST'] );
+	define( 'WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST'] . '/wp' );
 
-	/**
-	 * Set Database Details
-	 */
-	define( 'DB_NAME', getenv( 'DB_NAME' ) );
-	define( 'DB_USER', getenv( 'DB_USER' ) );
-	define( 'DB_PASSWORD', getenv( 'DB_PASSWORD' ) !== false ? getenv( 'DB_PASSWORD' ) : '' );
-	define( 'DB_HOST', getenv( 'DB_HOST' ) );
+}
 
-	/**
-	 * Set debug modes
-	 */
-	define( 'WP_DEBUG', getenv( 'WP_DEBUG' ) === 'true' ? true : false );
-	define( 'IS_LOCAL', getenv( 'IS_LOCAL' ) !== false ? true : false );
+// Don't show deprecations; useful under PHP 5.5
+error_reporting(E_ALL ^ E_DEPRECATED);
+/** Define appropriate location for default tmp directory on Pantheon */
+define('WP_TEMP_DIR', $_SERVER['HOME'] .'/tmp');
 
-	/**#@+
-	 * Authentication Unique Keys and Salts.
-	 *
-	 * Change these to different unique phrases!
-	 * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-	 * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-	 *
-	 * @since 2.6.0
-	 */
-	define( 'AUTH_KEY',         'put your unique phrase here' );
-	define( 'SECURE_AUTH_KEY',  'put your unique phrase here' );
-	define( 'LOGGED_IN_KEY',    'put your unique phrase here' );
-	define( 'NONCE_KEY',        'put your unique phrase here' );
-	define( 'AUTH_SALT',        'put your unique phrase here' );
-	define( 'SECURE_AUTH_SALT', 'put your unique phrase here' );
-	define( 'LOGGED_IN_SALT',   'put your unique phrase here' );
-	define( 'NONCE_SALT',       'put your unique phrase here' );
-
+// Force the use of a safe temp directory when in a container
+if ( defined( 'PANTHEON_BINDING' ) ):
+	define( 'WP_TEMP_DIR', sprintf( '/srv/bindings/%s/tmp', PANTHEON_BINDING ) );
 endif;
 
-/*
- * If on Pantheon
+// FS writes aren't permitted in test or live, so we should let WordPress know to
+// disable relevant UI.
+if (in_array($_ENV['PANTHEON_ENVIRONMENT'], array( 'test', 'live' ) ) ) {
+    if ( ! defined('DISALLOW_FILE_MODS') ) {
+        define( 'DISALLOW_FILE_MODS', true );
+    }
+    if ( ! defined('DISALLOW_FILE_EDIT') ) {
+        define( 'DISALLOW_FILE_EDIT', true );
+    }	
+}
+
+/**
+ * Set WP_ENVIRONMENT_TYPE according to the Pantheon Environment
  */
-if ( isset( $_ENV['PANTHEON_ENVIRONMENT'] ) ):
+if (getenv('WP_ENVIRONMENT_TYPE') === false) {
+    switch ($_ENV['PANTHEON_ENVIRONMENT']) {
+        case 'live':
+            putenv('WP_ENVIRONMENT_TYPE=production');
+            break;
+        case 'test':
+            putenv('WP_ENVIRONMENT_TYPE=staging');
+            break;
+        default:
+            putenv('WP_ENVIRONMENT_TYPE=development');
+            break;
+    }
+}
 
-	// ** MySQL settings - included in the Pantheon Environment ** //
-	/** The name of the database for WordPress */
-	define( 'DB_NAME', $_ENV['DB_NAME'] );
+/**
+ * Force SSL
+ */
+if ( ! defined('FORCE_SSL_ADMIN') ) {
+    define( 'FORCE_SSL_ADMIN', true );
+}
 
-	/** MySQL database username */
-	define( 'DB_USER', $_ENV['DB_USER'] );
+/**
+ * Defaults you may override
+ *
+ * To override, define your constant in your wp-config.php before wp-config-pantheon.php is required.
+ */
 
-	/** MySQL database password */
-	define( 'DB_PASSWORD', $_ENV['DB_PASSWORD'] );
-
-	/** MySQL hostname; on Pantheon this includes a specific port number. */
-	define( 'DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT'] );
-
-	/** Database Charset to use in creating database tables. */
-	define( 'DB_CHARSET', 'utf8' );
-
-	/** The Database Collate type. Don't change this if in doubt. */
-	define( 'DB_COLLATE', '' );
-
-	/**#@+
-	 * Authentication Unique Keys and Salts.
-	 *
-	 * Change these to different unique phrases!
-	 * You can generate these using the {@link https://api.wordpress.org/secret-key/1.1/salt/ WordPress.org secret-key service}
-	 * You can change these at any point in time to invalidate all existing cookies. This will force all users to have to log in again.
-	 *
-	 * Pantheon sets these values for you also. If you want to shuffle them you
-	 * can do so via your dashboard.
-	 *
-	 * @since 2.6.0
-	 */
-	define( 'AUTH_KEY', $_ENV['AUTH_KEY'] );
-	define( 'SECURE_AUTH_KEY', $_ENV['SECURE_AUTH_KEY'] );
-	define( 'LOGGED_IN_KEY', $_ENV['LOGGED_IN_KEY'] );
-	define( 'NONCE_KEY', $_ENV['NONCE_KEY'] );
-	define( 'AUTH_SALT', $_ENV['AUTH_SALT'] );
-	define( 'SECURE_AUTH_SALT', $_ENV['SECURE_AUTH_SALT'] );
-	define( 'LOGGED_IN_SALT', $_ENV['LOGGED_IN_SALT'] );
-	define( 'NONCE_SALT', $_ENV['NONCE_SALT'] );
-	/**#@-*/
-
-	/** A couple extra tweaks to help things run well on Pantheon. **/
-	if ( isset( $_SERVER['HTTP_HOST'] ) ) {
-		// HTTP is still the default scheme for now.
-		$scheme = 'http';
-		// If we have detected that the end use is HTTPS, make sure we pass that
-		// through here, so <img> tags and the like don't generate mixed-mode
-		// content warnings.
-		if ( isset( $_SERVER['HTTP_USER_AGENT_HTTPS'] ) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON' ) {
-			$scheme = 'https';
-		}
-		define( 'WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST'] );
-		define( 'WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST'] . '/wp' );
-
-	}
-
-	// Force the use of a safe temp directory when in a container
-	if ( defined( 'PANTHEON_BINDING' ) ):
-		define( 'WP_TEMP_DIR', sprintf( '/srv/bindings/%s/tmp', PANTHEON_BINDING ) );
-	endif;
-
-	// FS writes aren't permitted in test or live, so we should let WordPress know to disable relevant UI
-	if ( in_array( $_ENV['PANTHEON_ENVIRONMENT'], array( 'test', 'live' ) ) && ! defined( 'DISALLOW_FILE_MODS' ) ) :
-		define( 'DISALLOW_FILE_MODS', true );
-	endif;
-
-endif;
+/** Disable wp-cron.php from running on every page load and rely on Pantheon to run cron via wp-cli */
+$network = isset($_ENV["FRAMEWORK"]) && $_ENV["FRAMEWORK"] === "wordpress_network";
+if ( ! defined( 'DISABLE_WP_CRON' ) && $network === false) {
+	define( 'DISABLE_WP_CRON', true );
+}
