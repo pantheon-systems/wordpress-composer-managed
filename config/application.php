f4bfd182b31d1ccbae6859566a0a86ede428d7e5
jspellman814 <john.spellman@pantheon.io>
2022-07-19 15:42:32 -0600

    Remove debugging settings section

--- config/application.php
+++ config/application.php
@@ -103,14 +103,6 @@ Config::define('NONCE_SALT', env('NONCE_SALT'));
 Config::define('WP_POST_REVISIONS', env('WP_POST_REVISIONS') ?: true);
 
 /**
- * Debugging Settings
- */
-Config::define('WP_DEBUG_DISPLAY', env('WP_DEBUG_DISPLAY') ?: false);
-Config::define('WP_DEBUG_LOG', env('WP_DEBUG_LOG') ?: false);
-Config::define('SCRIPT_DEBUG', env('SCRIPT_DEBUG') ?: false);
-ini_set('display_errors', '0');
-
-/**
  * Allow WordPress to detect HTTPS when used behind a reverse proxy or a load balancer
  * See https://codex.wordpress.org/Function_Reference/is_ssl#Notes
  */
