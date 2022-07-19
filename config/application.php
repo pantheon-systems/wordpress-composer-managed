a73843116303cbc10ac72ff9016e68d45c87c351
jspellman814 <john.spellman@pantheon.io>
2022-07-19 15:32:14 -0600

    Use .env.patheon

--- config/application.php
+++ config/application.php
@@ -43,15 +43,15 @@ if ( isset( $_SERVER['HTTP_HOST'] ) ) {
 }
 
 /**
- * Use Dotenv to set required environment variables and load .env file in root
- * .env.local will override .env if it exists
+ * Use Dotenv to set required environment variables and load .env.pantheon file in root
+ * .env.local will override .env.pantheon if it exists
  */
 $env_files = file_exists($root_dir . '/.env.local')
-    ? ['.env', '.env.local']
-    : ['.env'];
+    ? ['.env.pantheon', '.env.local']
+    : ['.env.pantheon'];
 
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable($root_dir, $env_files, false);
-if (file_exists($root_dir . '/.env')) {
+if (file_exists($root_dir . '/.env.pantheon')) {
     $dotenv->load();
     if (!env('DATABASE_URL')) {
         $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD']);
