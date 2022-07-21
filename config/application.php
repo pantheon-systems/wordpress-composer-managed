5bf3d2f0fe941dc9c8a884a835a099a7d5ba78c0
jspellman814 <john.spellman@pantheon.io>
2022-07-21 13:12:38 -0600

    Try just define

--- config/application.php
+++ config/application.php
@@ -67,6 +67,9 @@ Config::define('DB_NAME', env('DB_NAME'));
 Config::define('DB_USER', env('DB_USER'));
 Config::define('DB_PASSWORD', env('DB_PASSWORD'));
 Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
+if (!isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+    Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
+}
 Config::define('DB_CHARSET', 'utf8mb4');
 Config::define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
