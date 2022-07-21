4ebd7c83a9cd0ba45ea61842d1047f10f57e56d1
jspellman814 <john.spellman@pantheon.io>
2022-07-21 11:05:18 -0600

    Add things back to wp-config-pantheon

--- config/application-pantheon.php
+++ config/application-pantheon.php
@@ -18,24 +18,3 @@ if (file_exists($root_dir . '/.env.pantheon')) {
         $dotenv->required(['DB_NAME', 'DB_USER', 'DB_PASSWORD']);
     }
 }
-
-if (isset($_SERVER['HTTP_HOST'])) {
-    // HTTP is still the default scheme for now.
-    $scheme = 'http';
-    // If we have detected that the end use is HTTPS, make sure we pass that
-    // through here, so <img> tags and the like don't generate mixed-mode
-    // content warnings.
-    if (isset($_SERVER['HTTP_USER_AGENT_HTTPS']) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON') {
-        $scheme = 'https';
-        $_SERVER['HTTPS'] = 'on';
-    }
-
-    Config::define('WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST']);
-    Config::define('WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST'] . '/wp');
-}
-
-$network = isset($_ENV["FRAMEWORK"]) && $_ENV["FRAMEWORK"] === "wordpress_network";
-/** Disable wp-cron.php from running on every page load and rely on Pantheon to run cron via wp-cli */
-if (!env('DISABLE_WP_CRON') && $network === false) {
-    Config::define('DISABLE_WP_CRON', true);
-}
