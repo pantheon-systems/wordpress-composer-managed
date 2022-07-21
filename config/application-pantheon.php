962f010171125689f3085a0e1084864a9049827a
jspellman814 <john.spellman@pantheon.io>
2022-07-21 12:55:00 -0600

    Remove some config (commit before is functioning PT)

--- config/application-pantheon.php
+++ config/application-pantheon.php
@@ -2,6 +2,10 @@
 
 use Roots\WPConfig\Config;
 
+Config::remove('WP_HOME');
+Config::remove('WP_SITEURL');
+Config::remove('DB_HOST');
+Config::remove('DISABLE_WP_CRON');
 Config::define('DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT']);
 
 Config::apply();
--- config/application.php
+++ config/application.php
@@ -1,6 +1,7 @@
 <?php
 /**
- * Your base production configuration goes in this file.
+ * Your base production configuration goes in this file. Environment-specific
+ * overrides go in their respective config/environments/{{WP_ENV}}.php file.
  *
  * A good default policy is to deviate from the production config as little as
  * possible. Try to define as much of your configuration in this file as you
@@ -47,6 +48,12 @@ if (file_exists($root_dir . '/.env')) {
 define('WP_ENV', env('WP_ENV') ?: 'production');
 
 /**
+ * URLs
+ */
+Config::define('WP_HOME', env('WP_HOME'));
+Config::define('WP_SITEURL', env('WP_SITEURL'));
+
+/**
  * Custom Content Directory
  */
 Config::define('CONTENT_DIR', '/app');
@@ -59,9 +66,7 @@ Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_D
 Config::define('DB_NAME', env('DB_NAME'));
 Config::define('DB_USER', env('DB_USER'));
 Config::define('DB_PASSWORD', env('DB_PASSWORD'));
-if (!env('DB_HOST')) {
-    Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
-}
+Config::define('DB_HOST', env('DB_HOST') ?: 'localhost');
 Config::define('DB_CHARSET', 'utf8mb4');
 Config::define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
@@ -91,6 +96,7 @@ Config::define('NONCE_SALT', env('NONCE_SALT'));
  * Custom Settings
  */
 Config::define('AUTOMATIC_UPDATER_DISABLED', true);
+Config::define('DISABLE_WP_CRON', env('DISABLE_WP_CRON') ?: false);
 // Disable the plugin and theme file editor in the admin
 Config::define('DISALLOW_FILE_EDIT', true);
 // Disable plugin and theme updates and installation from the admin
