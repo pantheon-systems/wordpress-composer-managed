b9702cfc6755afc592963ef2884400edd77e3f2f
jspellman814 <john.spellman@pantheon.io>
2022-07-21 11:08:59 -0600

    More tweaks

--- web/wp-config-pantheon.php
+++ web/wp-config-pantheon.php
@@ -10,20 +10,6 @@
  * when attempting to apply upstream updates.
  */
 
-/** A couple extra tweaks to help things run well on Pantheon. **/
-if (isset($_SERVER['HTTP_HOST'])) {
-    // HTTP is still the default scheme for now.
-    $scheme = 'http';
-    // If we have detected that the end use is HTTPS, make sure we pass that
-    // through here, so <img> tags and the like don't generate mixed-mode
-    // content warnings.
-    if (isset($_SERVER['HTTP_USER_AGENT_HTTPS']) && $_SERVER['HTTP_USER_AGENT_HTTPS'] == 'ON') {
-        $scheme = 'https';
-        $_SERVER['HTTPS'] = 'on';
-    }
-    define('WP_HOME', $scheme . '://' . $_SERVER['HTTP_HOST']);
-    define('WP_SITEURL', $scheme . '://' . $_SERVER['HTTP_HOST']);
-}
 // Don't show deprecations; useful under PHP 5.5
 error_reporting(E_ALL ^ E_DEPRECATED);
 /** Define appropriate location for default tmp directory on Pantheon */
@@ -49,16 +35,4 @@ if (getenv('WP_ENVIRONMENT_TYPE') === false) {
             putenv('WP_ENVIRONMENT_TYPE=development');
             break;
     }
-}
-
-/**
- * Defaults you may override
- *
- * To override, define your constant in your wp-config.php before wp-config-pantheon.php is required.
- */
-
-/** Disable wp-cron.php from running on every page load and rely on Pantheon to run cron via wp-cli */
-$network = isset($_ENV["FRAMEWORK"]) && $_ENV["FRAMEWORK"] === "wordpress_network";
-if ( ! defined( 'DISABLE_WP_CRON' ) && $network === false) {
-	define( 'DISABLE_WP_CRON', true );
 }
