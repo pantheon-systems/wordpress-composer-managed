a284b906eb0fe225b124425c40be93c12aeec249
jspellman814 <john.spellman@pantheon.io>
2022-07-21 12:03:27 -0600

    Define in app.php

--- config/application-pantheon.php
+++ config/application-pantheon.php
@@ -2,6 +2,6 @@
 
 use Roots\WPConfig\Config;
 
-Config::define('DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT']);
 
-Config::apply();
+
+// Config::apply();
--- config/application.php
+++ config/application.php
@@ -59,6 +59,7 @@ Config::define('WP_CONTENT_URL', Config::get('WP_HOME') . Config::get('CONTENT_D
 Config::define('DB_NAME', env('DB_NAME'));
 Config::define('DB_USER', env('DB_USER'));
 Config::define('DB_PASSWORD', env('DB_PASSWORD'));
+Config::define('DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT']);
 Config::define('DB_CHARSET', 'utf8mb4');
 Config::define('DB_COLLATE', '');
 $table_prefix = env('DB_PREFIX') ?: 'wp_';
