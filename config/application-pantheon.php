a4b257bf2f8cc179b6ed47739ab4243d58337fff
jspellman814 <john.spellman@pantheon.io>
2022-07-21 12:58:07 -0600

    Try not removing

--- config/application-pantheon.php
+++ config/application-pantheon.php
@@ -1,11 +1,17 @@
 <?php
+/**
+ * Pantheon platform settings.
+ *
+ * IMPORTANT NOTE:
+ * Do not modify this file. This file is maintained by Pantheon.
+ *
+ * Site-specific modifications belong in wp-config.php, not this file. This
+ * file may change in future releases and modifications would cause conflicts
+ * when attempting to apply upstream updates.
+ */
 
 use Roots\WPConfig\Config;
-
-Config::remove('WP_HOME');
-Config::remove('WP_SITEURL');
-Config::remove('DB_HOST');
-Config::remove('DISABLE_WP_CRON');
+å
 Config::define('DB_HOST', $_ENV['DB_HOST'] . ':' . $_ENV['DB_PORT']);
 
 Config::apply();
