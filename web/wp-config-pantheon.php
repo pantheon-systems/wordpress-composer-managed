9400e0a21a8d082fee98ba4c2c7effc480d496fb
jspellman814 <john.spellman@pantheon.io>
2022-07-25 13:17:24 -0600

    Remove DISALLOW_FILE_MODS from wp-config-pantheon

--- web/wp-config-pantheon.php
+++ web/wp-config-pantheon.php
@@ -33,11 +33,6 @@ error_reporting(E_ALL ^ E_DEPRECATED);
 /** Define appropriate location for default tmp directory on Pantheon */
 define('WP_TEMP_DIR', sys_get_temp_dir());
 
-// FS writes aren't permitted in test or live, so we should let WordPress know to disable relevant UI
-if (in_array($_ENV['PANTHEON_ENVIRONMENT'], array( 'test', 'live' )) && ! env('DISALLOW_FILE_MODS')) {
-    Config::define('DISALLOW_FILE_MODS', true);
-}
-
 /**
  * Set WP_ENVIRONMENT_TYPE according to the Pantheon Environment
  */
