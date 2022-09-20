560e84ea853ba025d481612264feac5d2cf82eb4
jspellman814 <john.spellman@pantheon.io>
2022-09-20 17:06:51 -0600

    Add wp-config-local-sample.php

--- web/wp-config.php
+++ web/wp-config.php
@@ -13,8 +13,18 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 /**
  * Pantheon platform settings. Everything you need should already be set.
  */
-if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT'])) {
+if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT']) && ($_ENV['PANTHEON_ENVIRONMENT'] !== 'lando')) {
 	require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
+
+/**
+ * Local configuration information.
+ *
+ * If you are working in a local/desktop development environment and want to
+ * keep your config separate, we recommend using a 'wp-config-local.php' file,
+ * which you should also make sure you .gitignore.
+ */
+} elseif (file_exists(dirname(__FILE__) . '/wp-config-local.php') && $_ENV['PANTHEON_ENVIRONMENT'] === 'lando'){
+	require_once(dirname(__FILE__) . '/wp-config-local.php');
 }
 
 require_once dirname(__DIR__) . '/config/application.php';
