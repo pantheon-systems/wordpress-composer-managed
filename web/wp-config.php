c3d1682b9bdb060b9b5413c671ec6ba8ec395631
jspellman814 <john.spellman@pantheon.io>
2022-09-21 10:24:44 -0600

    Use .env.local for local environment configuration

--- web/wp-config.php
+++ web/wp-config.php
@@ -1,7 +1,9 @@
 <?php
 /**
  * This is where you should at your configuration customizations. It will work out of the box on Pantheon
- * but you may find there are a lot of neat tricks to be used here.
+ * but you may find there are a lot of neat tricks to be used here.'
+ * 
+ * For local development, see .env.local-sample.
  *
  * See our documentation for more details:
  *
@@ -13,18 +15,8 @@ require_once dirname(__DIR__) . '/vendor/autoload.php';
 /**
  * Pantheon platform settings. Everything you need should already be set.
  */
-if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT']) && ($_ENV['PANTHEON_ENVIRONMENT'] !== 'lando')) {
+if (file_exists(dirname(__FILE__) . '/wp-config-pantheon.php') && isset($_ENV['PANTHEON_ENVIRONMENT']) && ('lando' !== $_ENV['PANTHEON_ENVIRONMENT'])) {
 	require_once(dirname(__FILE__) . '/wp-config-pantheon.php');
-
-/**
- * Local configuration information.
- *
- * If you are working in a local/desktop development environment and want to
- * keep your config separate, we recommend using a 'wp-config-local.php' file,
- * which you should also make sure you .gitignore.
- */
-} elseif (file_exists(dirname(__FILE__) . '/wp-config-local.php') && $_ENV['PANTHEON_ENVIRONMENT'] === 'lando'){
-	require_once(dirname(__FILE__) . '/wp-config-local.php');
 }
 
 require_once dirname(__DIR__) . '/config/application.php';
