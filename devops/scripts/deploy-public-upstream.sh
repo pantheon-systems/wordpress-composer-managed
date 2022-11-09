171b98d22500575322a29b258ead38e68621950f
Chris Reynolds <chris.reynolds@pantheon.io>
2022-11-09 14:40:52 -0700

    Skip commits with nonrelaese changes (#52)
    * if a commit has release and nonrelease changes, don't include it
    
    * add an extra comment about non-release changes
    
    * update changelog

--- devops/scripts/deploy-public-upstream.sh
+++ devops/scripts/deploy-public-upstream.sh
@@ -32,8 +32,16 @@ for commit in $newcommits; do
   fi
 
   if [[ $commit_type == "mixed" ]] ; then
-    2>&1 echo "Commit ${commit} contains both release and nonrelease changes. Cannot proceed."
-    exit 1
+    2>&1 echo "Commit ${commit} contains both release and nonrelease changes. Skipping this commit."
+    echo "You may wish to ensure that nothing in this commit is meant for release."
+    delete=(${commit})
+    for remove in "${delete[@]}"; do
+      for i in "${commits[@]}"; do
+        if [ [ ${commits[i]} = $remove ]]; then
+          unset 'commits[i]'
+        fi
+      done
+    done
   fi
 done
 
