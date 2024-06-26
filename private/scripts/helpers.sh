4361341ea12914ff4800cc174cde96f9be0c0b66
Chris Reynolds <chris@jazzsequence.com>
2024-06-26 12:32:51 -0600

    set the default php version to 8.1

--- private/scripts/helpers.sh
+++ private/scripts/helpers.sh
@@ -24,7 +24,7 @@ sitename="${SITENAME:-}"
 sftpuser="${SFTPUSER:-}"
 sftphost="${SFTPHOST:-}"
 sagename="${SAGENAME:-}"
-phpversion="${PHPVERSION:-8.0}"
+phpversion="${PHPVERSION:-8.1}"
 is_ci="${CI:-0}"
 siteenv="${SITEENV:-dev}"
 if [ "$siteenv" == "dev" ]; then
