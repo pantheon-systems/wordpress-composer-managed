e37c6b5477ebf045765267b52fc25da4de761a9d
Chris Reynolds <chris@jazzsequence.com>
2024-07-10 11:54:18 -0600

    delete wp-config-pantheon

--- web/wp-config-pantheon.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Pantheon wp-config file.
- *
- * This file is an artifact and has no content. It is not loaded by wp-config (Pantheon-specific configurations have been moved to config/application.pantheon.php).
- *
- * This file can be deleted and will never be updated.
- * If you're interested in an example of a wp-config-pantheon.php file for reference, see https://github.com/pantheon-systems/WordPress/blob/default/wp-config-pantheon.php.
- */
