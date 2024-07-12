de3dce1a4f5b003477c276a642eee88329523058
Chris Reynolds <chris@jazzsequence.com>
2024-07-12 11:10:48 -0600

    shellcheck

--- devops/scripts/run-playwright-tests.sh
+++ devops/scripts/run-playwright-tests.sh
@@ -46,12 +46,12 @@ log_into_terminus() {
 create_site() {
   echo ""
   echo -e "${YELLOW}Create ${site_id} if it does not exist.${RESET}"
-  if terminus site:info ${site_id}; then
+  if terminus site:info "${site_id}"; then
     echo "Test site already exists, skipping site creation."
     # If the site exists already, we should switch it to git mode.
-    terminus connection:set ${site_id}.dev git -y
+    terminus connection:set "${site_id}".dev git -y
   else
-    terminus site:create ${site_id} "${site_name}" ${UPSTREAM_ID} --org=5ae1fa30-8cc4-4894-8ca9-d50628dcba17
+    terminus site:create "${site_id}" "${site_name}" ${UPSTREAM_ID} --org=5ae1fa30-8cc4-4894-8ca9-d50628dcba17
   fi
 }
 
@@ -61,7 +61,7 @@ clone_site() {
   echo "Setting up some git config..."
   git config --global user.email "cms-platform+sage-testing@pantheon.io"
   git config --global user.name "Pantheon WPCM Bot"
-  terminus local:clone ${site_id}
+  terminus local:clone "${site_id}"
 }
 
 copy_multisite_config() {
@@ -71,19 +71,19 @@ copy_multisite_config() {
 
   echo ""
   echo -e "${YELLOW}Copying multisite application.php${RESET}"
-  rm ${workspace}/config/application.php
-  cp ${workspace}/.github/fixtures/config/application.${type}.php ~/pantheon-local-copies/${site_id}/config/
-  mv ~/pantheon-local-copies/${site_id}/config/application.${type}.php ~/pantheon-local-copies/${site_id}/config/application.php
-  cd ~/pantheon-local-copies/${site_id}
-  git add ~/pantheon-local-copies/${site_id}/config/application.php
+  rm "${workspace}"/config/application.php
+  cp "${workspace}"/.github/fixtures/config/application."${type}".php ~/pantheon-local-copies/"${site_id}"/config/
+  mv ~/pantheon-local-copies/"${site_id}"/config/application."${type}".php ~/pantheon-local-copies/"${site_id}"/config/application.php
+  cd ~/pantheon-local-copies/"${site_id}"
+  git add ~/pantheon-local-copies/"${site_id}"/config/application.php
   git commit -m "Set up ${type} multisite config" || true
 }
 
 copy_pr_updates() {
   echo "Commit Message: ${commit_msg}"
-  cd ~/pantheon-local-copies/${site_id}
+  cd ~/pantheon-local-copies/"${site_id}"
   echo -e "${YELLOW}Copying latest changes and committing to the site.${RESET}"
-  rsync -a --exclude='.git' ${workspace}/ .
+  rsync -a --exclude='.git' "${workspace}"/ .
   git add -A
   git commit -m "Update to latest commit: ${commit_msg}" || true
   git push origin master || true
@@ -92,25 +92,24 @@ copy_pr_updates() {
 status_check() {
   echo ""
   echo -e "${YELLOW}Checking WordPress install status${RESET}"
-  terminus wp ${site_id}.dev -- cli info
-  if [ ${type} != 'single' ]; then
-    terminus wp ${site_id}.dev -- config get MULTISITE # Should be true.
-    if [ $? -ne 0 ]; then
+  terminus wp "${site_id}".dev -- cli info
+  if [ "${type}" != 'single' ]; then
+    if ! terminus wp "${site_id}".dev -- config get MULTISITE; then
       echo -e "${RED}Multisite not found!"
       exit 1
     fi
 
-    SUBDOMAIN_INSTALL=$(terminus wp ${site_id}.dev -- config get SUBDOMAIN_INSTALL)
-    if [ ${type} == 'subdir' ]; then
+    SUBDOMAIN_INSTALL=$(terminus wp "${site_id}".dev -- config get SUBDOMAIN_INSTALL)
+    if [ "${type}" == 'subdir' ]; then
       # SUBDOMAIN_INSTALL should be false.
-      if [ $SUBDOMAIN_INSTALL != 0 ]; then
+      if [ "$SUBDOMAIN_INSTALL" != 0 ]; then
         echo -e "${RED}Subdirectory configuration not found!"
         exit 1
       fi
     fi
-    if [ ${type} == 'subdom' ]; then
+    if [ "${type}" == 'subdom' ]; then
       # SUBDOMAIN_INSTALL should be true.
-      if [ $SUBDOMAIN_INSTALL != 1 ]; then
+      if [ "$SUBDOMAIN_INSTALL" != 1 ]; then
         echo -e "${RED}Subdomain configuration not found!"
         exit 1
       fi
@@ -119,33 +118,33 @@ status_check() {
 }
 
 install_wp() {
-  terminus wp ${site_id}.dev -- db reset
+  terminus wp "${site_id}".dev -- db reset
   echo ""
   # Single site.
-  if [ ${type} == 'single' ]; then
+  if [ "${type}" == 'single' ]; then
     echo -e "${YELLOW}Install (Single Site) WordPress${RESET}"
-    terminus wp ${site_id}.dev -- core install --title="${site_name}" --admin_user=wpcm --admin_email=test@dev.null
+    terminus wp "${site_id}".dev -- core install --title="${site_name}" --admin_user=wpcm --admin_email=test@dev.null
   fi
 
   # Subdirectory multisite.
-  if [ ${type} == 'subdir' ]; then
+  if [ "${type}" == 'subdir' ]; then
     echo -e "${YELLOW}Install (Subdirectory Multisite) WordPress${RESET}"
-    terminus wp ${site_id}.dev -- core multisite-install --title="${site_name}" --admin_user=wpcm --admin_email=test@dev.null --subdomains=false
+    terminus wp "${site_id}".dev -- core multisite-install --title="${site_name}" --admin_user=wpcm --admin_email=test@dev.null --subdomains=false
   fi
 
   # Subdomain multisite.
-  if [ ${type} == 'subdom' ]; then
+  if [ "${type}" == 'subdom' ]; then
     echo -e "${YELLOW}Install (Subdomain Multisite) WordPress${RESET}"
-    terminus wp ${site_id}.dev -- core multisite-install --title="${site_name}" --admin_user=wpcm --admin_email=test@dev.null --subdomains=true
+    terminus wp "${site_id}".dev -- core multisite-install --title="${site_name}" --admin_user=wpcm --admin_email=test@dev.null --subdomains=true
   fi
 
-  terminus wp ${site_id}.dev -- option update permalink_structure '/%postname%/'
-  terminus wp ${site_id}.dev -- rewrite flush
-  terminus wp ${site_id}.dev -- cache flush
+  terminus wp "${site_id}".dev -- option update permalink_structure '/%postname%/'
+  terminus wp "${site_id}".dev -- rewrite flush
+  terminus wp "${site_id}".dev -- cache flush
 }
 
 set_up_subsite() {
-  if [ ${type} == 'single' ]; then
+  if [ "${type}" == 'single' ]; then
     exit
   fi
 
@@ -160,30 +159,30 @@ set_up_subsite() {
   fi
 
   # Check if the sub-site already exists.
-  EXISTING_SITE=$(terminus wp ${site_id}.dev -- site list --field=url | grep -w "foo")
+  EXISTING_SITE=$(terminus wp "${site_id}".dev -- site list --field=url | grep -w "foo")
 
   if [ -z "$EXISTING_SITE" ]; then
     # Create the sub-site only if it does not already exist.
-    terminus wp ${site_id}.dev -- site create --slug=foo --title="Foo" --email="foo@dev.null"
-    terminus wp ${site_id}.dev -- option update permalink_structure '/%postname%/' --url="$URL"
+    terminus wp "${site_id}".dev -- site create --slug=foo --title="Foo" --email="foo@dev.null"
+    terminus wp "${site_id}".dev -- option update permalink_structure '/%postname%/' --url="$URL"
   else
     echo -e "${YELLOW}Sub-site already exists at $URL. Skipping creation.${RESET}"
   fi
-  terminus wp ${site_id}.dev -- option update permalink_structure '/%postname%/' --url="$URL"
+  terminus wp "${site_id}".dev -- option update permalink_structure '/%postname%/' --url="$URL"
 }
 
 install_wp_graphql() {
-  terminus workflow:wait ${site_id}.dev
-  terminus connection:set ${site_id}.dev sftp
+  terminus workflow:wait "${site_id}".dev
+  terminus connection:set "${site_id}".dev sftp
   echo ""
   echo -e "${YELLOW}Install WP GraphQL${RESET}"
-  terminus wp ${site_id}.dev -- plugin install --activate wp-graphql
+  terminus wp "${site_id}".dev -- plugin install --activate wp-graphql
   if [ "${type}" == 'subdom' ]; then
     # We're going to assume the foo.dev subdomain already exists.
-    terminus wp ${site_id}.dev -- plugin activate wp-graphql --url=foo.dev-${site_id}.pantheonsite.io
+    terminus wp "${site_id}".dev -- plugin activate wp-graphql --url=foo.dev-"${site_id}".pantheonsite.io
   fi
   if [ "${type}" == 'subdir' ]; then
-    terminus wp ${site_id}.dev -- plugin activate wp-graphql --url=${site_url}/foo
+    terminus wp "${site_id}".dev -- plugin activate wp-graphql --url="${site_url}"/foo
   fi
 }
 
@@ -192,13 +191,13 @@ run_playwright() {
   echo -e "${BLUE}Running Playwright tests${RESET}"
   npm run test .github/tests/wpcm.spec.ts
 
-  if [ ${type} == 'subdir' ]; then
+  if [ "${type}" == 'subdir' ]; then
     SITE_URL="${site_url}/foo"
     echo -e "${BLUE}Running Playwright tests on WordPress subdirectory subsite${RESET}"
     npm run test .github/tests/wpcm.spec.ts
   fi
 
-  if [ ${type} == 'subdom' ]; then
+  if [ "${type}" == 'subdom' ]; then
     SITE_URL="https://foo.dev-${site_id}.pantheonsite.io"
     echo -e "${BLUE}Running Playwright tests on WordPress subdomain subsite${RESET}"
     npm run test .github/tests/wpcm.spec.ts
